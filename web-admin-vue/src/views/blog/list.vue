557da1e305e6e67180aed7dcb0cbde80ca07229b
wangleihh <710815126@qq.com>
2019-05-21 07:40:07 +0800

    编辑页面

--- web-admin-vue/src/views/blog/list.vue
+++ web-admin-vue/src/views/blog/list.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    <el-table :data="tableData">
-      <el-table-column prop="id" label="序号" width="140"></el-table-column>
-      <el-table-column prop="title" label="标题" width="120"></el-table-column>
-      <el-table-column label="操作">
+    <!-- 搜索 -->
+    <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+      <el-form :inline="true">
+        <el-form-item>
+          <el-select v-model="value" clearable placeholder="状态">
+            <el-option
+              v-for="item in status"
+              :key="item.statusId"
+              :label="item.label"
+              :value="item.statusId"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="姓名" v-model="searchName"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="doFilter()">
+            <i class="el-icon-search"></i>搜索
+          </el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">新增</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+    <!-- 列表 -->
+    <el-table :data="tableList" v-loading="listLoading" border element-loading-text="拼命加载中" style="width: 100%;">
+      <el-table-column prop="id" label="序号" width="65">
+      </el-table-column>
+      <el-table-column prop="title" label="标题">
+      </el-table-column>
+      <el-table-column prop="title" min-width="150px" label="标题">
+      </el-table-column>
+      <el-table-column prop="viewCount" label="阅读数" width="65">
+      </el-table-column>
+      <el-table-column prop="updateTime" label="修改时间" width="160px">
+      </el-table-column>
+      <el-table-column :formatter="formatCode" label="状态" width="120">
+      </el-table-column>
+      <el-table-column prop="operation" label="操作 ">
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.row.id)">编辑</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.row.id)">删除</el-button>
+          <el-button size="small" type="primary" @click="handleUpdate(scope.row)">编辑</el-button>
+          <el-button size="small" type="danger" @click="deleteUpdate(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -20,42 +57,54 @@ export default {
 
   components: {},
 
-  created(){
-    this.getBlogs()
+  created() {
+    this.getBlogs();
   },
   data() {
     return {
-      tableData: []
+      tableData: [],
+      status: [
+        {
+          statusId: 1,
+          label: "启用"
+        },
+        {
+          statusId: 2,
+          label: "禁用"
+        },
+      ]
     };
   },
   methods: {
+    doFilter() {
+
+    },
     getBlogs() {
       getBlogs().then(res => {
         if (res.data.resultCode == 200) {
-          this.tableData = res.data.resultJson
+          this.tableData = res.data.resultJson;
         }
       });
     },
-    handleDelete(id){
-       deleteBlogById({id}).then(res => {
-          if (res.data.resultCode == 200) {
-           this.$message({
-            message: res.data.resultMessage || '删除成功',
+    handleDelete(id) {
+      deleteBlogById({ id }).then(res => {
+        if (res.data.resultCode == 200) {
+          this.$message({
+            message: res.data.resultMessage || "删除成功",
             type: "success",
             showClose: true,
             duration: 1000
           });
           this.getBlogs();
-        }else {
-            this.$message({
-            message: res.data.resultMessage || '删除失败，请联系管理员',
+        } else {
+          this.$message({
+            message: res.data.resultMessage || "删除失败，请联系管理员",
             type: "error",
             showClose: true,
             duration: 1000
           });
         }
-      }) 
-      
+      });
     }
   }
 };
