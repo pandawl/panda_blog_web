34da8ba15bd5c2fe6285390f58400811ed3740d6
wanglei <710815126@qq.com>
2019-05-23 18:50:53 +0800

    blog 编辑页面

--- web-vue/src/components/HeaderMenus.vue
+++ web-vue/src/components/HeaderMenus.vue
@@ -1,23 +1,53 @@
 <template>
-  <div>
-    <el-menu
-      class="header_panda"
-      mode="horizontal"
-      background-color="#1E9FFF"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      router
-    >
-      <el-menu-item class="header_index_panda" index="/named_view">网站首页</el-menu-item>
-      <el-menu-item index="/login">技术杂谈</el-menu-item>
-      <el-menu-item index="3">生活笔记</el-menu-item>
-      <el-menu-item index="4">日志系统</el-menu-item>
-      <el-menu-item index="4">我的书单</el-menu-item>
-      <el-menu-item index="5">留言板</el-menu-item>
-      <el-menu-item index="6">关于我</el-menu-item>
+  
 
-    </el-menu>
+<div>
+  
+<!--top begin-->
+<header id="header">
+  <div class="navbox">
+    <h2 id="mnavh"><span class="navicon"></span></h2>
+    <div class="logo"><a href="http://www.yangqq.com">杨青青个人博客</a></div>
+    <nav>
+      <ul id="starlist">
+        <li><a href="index.html">首页</a></li>
+        <li><a href="list.html">个人博客日记</a></li>
+        <li class="menu"><a href="list2.html">网站制作</a>
+          <ul class="sub">
+            <li><a href="/6">推荐工具</a></li>
+            <li><a href="/7">JS经典实例</a></li>
+            <li><a href="/8">网站建设</a></li>
+            <li><a href="/7">CSS3|Html5</a></li>
+            <li><a href="/8">心得笔记</a></li>
+          </ul>
+          <span></span></li>
+        <li><a href="list3.html">网页设计心得</a></li>
+        <li><a href="daohang.html">优秀个人博客</a></li>
+        <li><a href="about.html">关于我</a></li>
+      </ul>
+    </nav>
+    <div class="searchico"></div>
   </div>
+</header>
+<div class="searchbox">
+  <div class="search">
+    <form action="/e/search/index.php" method="post" name="searchform" id="searchform">
+      <input name="keyboard" id="keyboard" class="input_text" value="请输入关键字词" style="color: rgb(153, 153, 153);" onfocus="if(value=='请输入关键字词'){this.style.color='#000';value=''}" onblur="if(value==''){this.style.color='#999';value='请输入关键字词'}" type="text">
+      <input name="show" value="title" type="hidden">
+      <input name="tempid" value="1" type="hidden">
+      <input name="tbname" value="news" type="hidden">
+      <input name="Submit" class="input_submit" value="搜索" type="submit">
+    </form>
+  </div>
+  <div class="searchclose"></div>
+</div>
+</div>
+
+
+
+
+
+  
 </template>
 <script>
 export default {
@@ -29,14 +59,5 @@ export default {
 </script>
 
 <style scoped>
-.header_index_panda {
-  margin-left: 16.7%;
-}
-.header_search_panda {
-  margin-left: 14%;
-}
-.index-select-button-panda{
-   background-color: aqua;
 
-}
 </style>
